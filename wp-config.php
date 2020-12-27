3ce8010042f9c97832507a37b70f47ffcfbb61b8
David DE CARVALHO <dedece35@gmail.com>
2020-12-28 00:10:46 +0100

    Mise en place log de debug

---
+++ wp-config.php
@@ -0,0 +1,12 @@
+// Activer le mode WP_DEBUG
+define('WP_DEBUG', true);
+
+// Activer l'enregistrement de débogage dans le fichier /wp-content/debug.log
+define('WP_DEBUG_LOG', true);
+
+// Désactiver l'affichage des erreurs et des alertes 
+// define('WP_DEBUG_DISPLAY', false);
+// @ini_set('display_errors',0);
+
+// Utiliser les versions de dev des fichiers JS et CSS du noyau (nécessaire seulement si vous modifiez ces fichiers)
+// define('SCRIPT_DEBUG', true);
